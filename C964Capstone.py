f3906fa62a8de27d089adca1c5a348e59c17dcfe
Harrison <47501747+Code-acus@users.noreply.github.com>
2023-03-11 13:13:19 -0700

    Update C964Capstone.py
    Complete overhaul of the code

--- C964Capstone.py
+++ C964Capstone.py
@@ -1,158 +1,102 @@
-import algorithm as algorithm
+# The code below is the code for the Diabetes Predictor app without the GUI.
+# It is a simple app that takes in the values for the 7 features and predicts whether the patient has diabetes or not.
+# The app is built using the scikit-learn library.
+
+import tkinter as tk
+from tkinter import messagebox
 import pandas as pd
-import plotly.express as px
-from scipy.stats._morestats import Mean
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report
+from sklearn import svm
 from sklearn.model_selection import train_test_split
 
-# Load data into a Pandas dataframe
-data = pd.read_csv('diabetes_data.csv')
+class App:
+    def __init__(self, master):
+        self.master = master
 
-# If the file is in a different directory, provide the full path to the file
-# data = pd.read_csv('/path/to/diabetes_data.csv')
+    def predict(self):
+        try:
+            input_data = {
+                'Pregnancies': [int(pregnancies_entry.get())],
+                'Glucose': [int(glucose_entry.get())],
+                'BloodPressure': [int(blood_pressure_entry.get())],
+                'SkinThickness': [int(skin_thickness_entry.get())],
+                'Insulin': [int(insulin_entry.get())],
+                'BMI': [float(bmi_entry.get())],
+                'DiabetesPedigreeFunction': [float(diabetes_pedigree_function_entry.get())]
+            }
+            input_df = pd.DataFrame(input_data)
+            prediction = App.predict_diabetes(input_df)
+            messagebox.showinfo("Prediction", "The patient is " + prediction)
+        except ValueError:
+            messagebox.showerror("Error", "Please enter valid values")
 
-# Data Wrangling
-processed_df = data.drop(columns=['Unnamed: 0'])  # Removing unnecessary column
-X = processed_df.drop(columns=['Outcome'])
-y = processed_df['Outcome']
+    def predict_diabetes(input_df):
+        # Load the dataset
+        df = pd.read_csv('diabetes.csv')
 
-# Split data into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+        # Split the data into training and testing sets
+        X = df.drop('Outcome', axis=1)
+        y = df['Outcome']
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-# Create a logistic regression model
-logreg = LogisticRegression(max_iter=1000)
+        # Train the model
+        model = svm.SVC(kernel='linear')
+        model.fit(X_train, y_train)
 
-# Fit the model to the training data
-logreg.fit(X_train, y_train)
+        # Make predictions
+        predictions = model.predict(X_test)
 
-# Make predictions on the test data
-y_pred = logreg.predict(X_test)
+        # Return the prediction
+        return model.predict(input_df)
 
-# Evaluate the model's performance
-accuracy = logreg.score(X_test, y_test)
-print("Accuracy:", accuracy)
+# Create the GUI
+root = tk.Tk()
+root.title("Diabetes Predictor")
 
-# Descriptive Method
-# Data Exploration
-fig1 = px.pie(processed_df, names='Outcome', title="Diabetes Outcome Distribution",
-              color_discrete_sequence=["green", "red"], category_orders={'Outcome': [0, 1]})
-fig1.show()
+# Create input fields and labels
+pregnancies_label = tk.Label(root, text="Pregnancies:")
+pregnancies_label.grid(row=0, column=0)
+pregnancies_entry = tk.Entry(root)
+pregnancies_entry.grid(row=0, column=1)
 
-fig2 = px.scatter_matrix(processed_df,
-                         dimensions=["Pregnancies", "Glucose", "BloodPressure", "SkinThickness", "Insulin", "BMI",
-                                     "DiabetesPedigreeFunction"],
-                         color="Outcome", title="Scatter Matrix of Diabetes Data",
-                         color_discrete_sequence=["green", "red"], category_orders={'Outcome': [0, 1]})
-fig2.show()
+glucose_label = tk.Label(root, text="Glucose:")
+glucose_label.grid(row=1, column=0)
+glucose_entry = tk.Entry(root)
+glucose_entry.grid(row=1, column=1)
 
-fig3 = px.scatter_matrix(df,
-    dimensions=["Worst Radius", "Worst Texture", "Worst Perimeter", "Worst Area", "Worst Smoothness", "Worst Compactness", "Worst Concavity"], color="Diagnosis",title="Worst Morphologies", color_discrete_sequence=["green","red"], category_orders={'Diagnosis': ['B', 'M']})
-fig3.update_layout(
-    width=950,
-    height=950,
-)
-fig3.show()
+blood_pressure_label = tk.Label(root, text="Blood Pressure:")
+blood_pressure_label.grid(row=2, column=0)
+blood_pressure_entry = tk.Entry(root)
+blood_pressure_entry.grid(row=2, column=1)
 
-# Data Inspection
+skin_thickness_label = tk.Label(root, text="Skin Thickness:")
+skin_thickness_label.grid(row=3, column=0)
+skin_thickness_entry = tk.Entry(root)
+skin_thickness_entry.grid(row=3, column=1)
 
-px.scatter(df, x='Area', y='Worst Area', color='Diagnosis', title="Typical v.s. Worst Area Morphology",
-              color_discrete_sequence=["green","red"], marginal_x="histogram", marginal_y="histogram")
+insulin_label = tk.Label(root, text="Insulin:")
+insulin_label.grid(row=4, column=0)
+insulin_entry = tk.Entry(root)
+insulin_entry.grid(row=4, column=1)
 
-# Data Exploration
+bmi_label = tk.Label(root, text="BMI:")
+bmi_label.grid(row=5, column=0)
+bmi_entry = tk.Entry(root)
+bmi_entry.grid(row=5, column=1)
 
-fig1 = px.pie(df, names='Diagnosis', title="Benign(B) V.S. Malignant(M) Case Sampling",
-              color_discrete_sequence=["green","red"], category_orders={'Diagnosis': ['B', 'M']})
-fig1.show()
+diabetes_pedigree_function_label = tk.Label(root, text="Diabetes Pedigree Function:")
+diabetes_pedigree_function_label.grid(row=6, column=0)
+diabetes_pedigree_function_entry = tk.Entry(root)
+diabetes_pedigree_function_entry.grid(row=6, column=1)
 
-fig2 = px.scatter_matrix(df,
-    dimensions=["Radius", "Texture", "Perimeter", "Area", "Smoothness", "Compactness", "Concavity"],
-                         color="Diagnosis",title="Typical Morphologies", color_discrete_sequence=["green","red"],
-                         category_orders={'Diagnosis': ['B', 'M']})
-fig2.update_layout(
-    width=950,
-    height=950,
-)
-fig2.show()
+# Create the predict button
+predict_button = tk.Button(root, text="Predict", command=App.predict)
+predict_button.grid(row=7, column=0, columnspan=2)
 
-fig3 = px.scatter_matrix(df,
-    dimensions=["Worst Radius", "Worst Texture", "Worst Perimeter", "Worst Area", "Worst Smoothness", "Worst Compactness", "Worst Concavity"], color="Diagnosis",title="Worst Morphologies", color_discrete_sequence=["green","red"], category_orders={'Diagnosis': ['B', 'M']})
-fig3.update_layout(
-    width=950,
-    height=950,
-)
-fig3.show()
+# Run the app
+app = App(root)
+root.mainloop()
 
-# Data Inspection
-
-px.scatter(df, x='Area', y='Worst Area', color='Diagnosis', title="Typical v.s. Worst Area Morphology",
-           color_discrete_sequence=["green","red"], marginal_x="hi")
-
-# Non-Descriptive Method
-# Machine Learning Algorithm
-# In this section, the machine learning algorithm is created and trained using the data collected in the previous section.
-# The algorithm is then tested and evaluated.
-
-X_train, X_test, y_train, y_test = train_test_split(model.drop('M',axis=1),
-                                                    model['M'], test_size=0.30,
-                                                    random_state=123)
-
-LogisticRegression(max_iter=600)
-
-# Support Vector Machine
-
-algorithm_2 = svm.SVC(gamma=1e-06, C=100000000.0)
-algorithm_2.fit(X_train,y_train)
-SVC(C=100000000.0, gamma=1e-06)
-
-# Evaluating Accuracy
-
-fna_diagnosis = algorithm.predict(X_test)
-print(classification_report(y_test,fna_diagnosis))
-
-print('Mean Absolute Error (MAE):', metrics.mean_absolute_error(y_test, fna_diagnosis))
-print('Mean Squared Error (MSE):', metrics.mean_squared_error(y_test, fna_diagnosis))
-print('Root-Mean-Square Error (RMSE):', np.sqrt(metrics.mean_squared_error(y_test, fna_diagnosis)))
-Mean Absolute Error (MAE): 13.444444444444445
-Mean Squared Error (MSE): 0.07602339181286549
-Root-Mean-Square Error (RMSE): 0.2757233972895037
-cm_1 = metrics.confusion_matrix(y_test, fna_diagnosis, labels=algorithm.classes_)
-disp_1 = metrics.ConfusionMatrixDisplay(confusion_matrix=cm_1, display_labels=algorithm.classes_)
-disp_1.plot();
-
-# Support Vector Machine
-
-fna_diagnosis_2 = algorithm_2.predict(X_test)
-print(classification_report(y_test,fna_diagnosis_2))
-
-print('Mean Absolute Error (MAE):', metrics.mean_absolute_error(y_test, fna_diagnosis_2))
-print('Mean Squared Error (MSE):', metrics.mean_squared_error(y_test, fna_diagnosis_2))
-print('Root-Mean-Square Error (RMSE):', np.sqrt(metrics.mean_squared_error(y_test, fna_diagnosis_2)))
-Mean Absolute Error (MAE): 11.988304093567251
-Mean Squared Error (MSE): 0.10526315789473684
-Root-Mean-Square Error (RMSE): 0.3244428422615251
-cm_2 = metrics.confusion_matrix(y_test, fna_diagnosis_2, labels=algorithm_2.classes_)
-disp_2 = metrics.ConfusionMatrixDisplay(confusion_matrix=cm_2, display_labels=algorithm_2.classes_)
-disp_2.plot();
-
-# Conclusion
-# Functionl Dashboard
-# The following code is used to create the functional dashboard.
-
-tab1 = HBox([input_container])
-tab2 = VBox([graph_container_1])
-tab3 = VBox([graph_container_2])
-tab4 = VBox([graph_container_3])
-tab = widgets.Tab(children=[tab1, tab2, tab3, tab4])
-tab.set_title(0, 'Morphology Input')
-tab.set_title(1, 'Reference Data')
-tab.set_title(2, 'Test Contrasting')
-tab.set_title(3, 'Accuracy Monitor')
-
-application_dashboard = VBox(children=[tab])
-
-# application_dashboard
-
-
-
-
+# Path: C964Capstone.py
+# The code below is the code for the Diabetes Predictor app without the GUI.
+# It is a simple app that takes in the values for the 7 features and predicts whether the patient has diabetes or not.
+# The app is built using the scikit-learn library.
